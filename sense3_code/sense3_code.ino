499b2d944ca867d1119211e13000ee4de0abd2e9
Brett Ian Balogh <brettbalogh@gmail.com>
2014-05-15 13:57:01 -0500

    commit

--- sense3_code/sense3_code.ino
+++ sense3_code/sense3_code.ino
@@ -3,14 +3,18 @@ Example Sound Level Sketch for the
 Adafruit Microphone Amplifier
 ****************************************/
 
-const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+const int sampleWindow = 100; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
 int val=0;
+int upperLimit=512;
+int lowerLimit=0;
 
 void setup() 
 {
+  analogReference(INTERNAL);
    pinMode(0, OUTPUT);
    pinMode(1, OUTPUT);
+   pinMode(A1, INPUT);
 }
 
 
@@ -39,8 +43,8 @@ void loop()
       }
    }
    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-   //constrain(peakToPeak, 0, 1023);
-   val = map(peakToPeak, 0, 1023, 0, 255);
-   analogWrite(0, val);
+   constrain(peakToPeak, lowerLimit, upperLimit);
+   val = map(peakToPeak, lowerLimit, upperLimit, 0, 255);
+   //analogWrite(0, val);
    analogWrite(1, 255-val);
 }
